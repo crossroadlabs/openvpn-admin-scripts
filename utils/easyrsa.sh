8dc685aaeb01cdd9a80dcaeb9b99ace8ef1eb412
Yehor Popovych <yehor@crossroad.io>
2019-10-10 16:57:15 +0300

    fixed easy-rsa call

--- utils/easyrsa.sh
+++ utils/easyrsa.sh
@@ -1,14 +1,13 @@
 
 function easyrsa_pki_path() {
-  DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-	source "$DIR/../config.sh"
-  echo $("$OPENVPN_CA_DIR/easyrsa" | grep -oP "PKI: \K(.*)")
+  info="$(easyrsa_call)"
+  echo $(echo "$info" | grep -oP "PKI: \K(.*)")
   return $?
 }
 
 function easyrsa_call() {
   DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 	source "$DIR/../config.sh"
-  echo $("$OPENVPN_CA_DIR/easyrsa" $@)
+  echo $(cd "$OPENVPN_CA_DIR" && "./easyrsa" $@)
   return $?
 }
